0c1f3f066993d23f2070ad3d99f575cd5039dbfc
Carl Malagodi <malagodicarl@gmail.com>
2022-04-15 15:04:35 -0500

    added routine for tuning drivestraight

--- src/v5_hal/firmware/include/auton/auton_routines/tuningRoutine.h
+++ src/v5_hal/firmware/include/auton/auton_routines/tuningRoutine.h
@@ -9,12 +9,13 @@
 
 class tuningRoutine : public Auton {
 public:
-    tuningRoutine(IDriveNode* drive_node);
+    tuningRoutine(IDriveNode* drive_node, OdometryNode* odometry_node);
 
     void AddNodes();
 
 private:
     IDriveNode* m_drive_node;
+    OdometryNode* m_odometry_node;
 
     AutonNode* m_forward_node;
     //AutonNode* m_backward_node;
--- src/v5_hal/firmware/src/auton/auton_routines/tuningRoutine.cpp
+++ src/v5_hal/firmware/src/auton/auton_routines/tuningRoutine.cpp
@@ -1,13 +1,14 @@
 #include "auton/auton_routines/tuningRoutine.h"
 
-tuningRoutine::tuningRoutine(IDriveNode* drive_node) : 
+tuningRoutine::tuningRoutine(IDriveNode* drive_node, OdometryNode* odometry_node) : 
         Auton("Tune DriveStraight"), 
-        m_drive_node(drive_node) {
+        m_drive_node(drive_node),
+        m_odometry_node(odometry_node){
     
 }
 
 void tuningRoutine::AddNodes() {
-    m_forward_node = new AutonNode(10, new DriveStraightAction(m_drive_node, 30, 50, 10));
+    m_forward_node = new AutonNode(10, new DriveStraightAction(m_drive_node, m_odometry_node, 25, 80, 80));
     Auton::AddFirstNode(m_forward_node);
 
     //m_backward_node = new AutonNode(10, new DriveStraightAction(m_drive_node, 800, 10000, 10000));

6c50a259b6a3da6b96a366368c5f576579295731
Dylan Powers <dylanpowers125@gmail.com>
2022-04-20 10:36:21 -0500

    created files for BackClawNode

---
+++ src/v5_hal/firmware/include/nodes/BackClawNode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "lib-rr/nodes/NodeManager.h"
+#include "lib-rr/nodes/sensor_nodes/ControllerNode.h"
+#include "lib-rr/nodes/actuator_nodes/ADIDigitalOutNode.h"
+#include "nodes/ClawNode.h"
+#include "pros/misc.h"
+
+class BackClawNode : public Node {
+private:
+
+    
+public:
+
+}

199767c664a6fa37fde26b566ef8506a947a67d5
Dylan Powers <dylanpowers125@gmail.com>
2022-04-20 20:53:42 -0500

    added node

--- src/v5_hal/firmware/include/nodes/BackClawNode.h
+++ src/v5_hal/firmware/include/nodes/BackClawNode.h
@@ -7,9 +7,43 @@
 #include "pros/misc.h"
 
 class BackClawNode : public Node {
-private:
-
-    
 public:
+    enum BackClawState {
+        PIVOT_BACK, PIVOT_DOWN_CLAW_OPEN, PIVOT_DOWN_CLAW_CLOSED
+    }
 
+    BackClawNode(NodeManager* node_manager, std::string handle_name,
+        ControllerNode* controller, pros::controller_digital_e_t pivotButton, 
+        pros::controller_digital_e_t clawButton, ADIDigitalOutNode* pivot
+        ADIDigitalOutNode* claw);
+
+    void initialize();
+
+    void setState(BackClawState state);
+
+    void togglePivot();
+
+    void toggleClaw();
+
+    void teleopPeriodic();
+    
+    void autonPeriodic();
+
+    ~BackClawNode();
+
+private:
+    BackClawState m_state;
+
+    ControllerNode* m_controller;
+    pros::controller_digital_e_t m_pivotButton;
+    pros::controller_digital_e_t m_clawButton;
+    ADIDigitalOutNode* m_pivot;
+    ADIDigitalOutNode* m_claw;
+
+    std::string m_handle_name;
+
+    bool m_pivotButtonPreivousState = false;
+    bool m_clawButtonPreviousState = false;
+
+    void periodic();
 }
